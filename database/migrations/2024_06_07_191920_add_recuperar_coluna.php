ae4504a53be934c0b9bd72ad907542e20525ca7c
DouglasHSM <doughenriqueps@gmail.com>
2024-06-07 16:42:24 -0300

    'teste'

---
+++ database/migrations/2024_06_07_191920_add_recuperar_coluna.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recupea', function (Blueprint $table) {
+            $table->id();
+            $table->string('email',150);
+            $table->string('codigo',150);
+            $table->datetime('tempo_limite');
+            $table->string('user_hash',255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recuperar');
+    }
+};
