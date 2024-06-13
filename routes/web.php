7ec1f44aa09960ecbd5327c4082f771878cda3cd
DouglasHSM <doughenriqueps@gmail.com>
2024-06-13 14:47:39 -0300

    v1.0

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [App\Http\Controllers\PrincipalController::class, 'principal']);
-Route::get('/contato', [App\Http\Controllers\ContatoController::class, 'contato']);
-Route::get('/sobre-nos', [App\Http\Controllers\SobreNosController::class, 'sobreNos']);
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/',[\App\Http\Controller\indexController::class,'index']);
+Route::get('/redirect',[\App\Http\Controller\indexController::class,'redirect']);
+Route::get('/contact',[\App\Http\Controller\indexController::class,'contact']);
+
+Route::get('/register',[\App\Http\Controller\indexController::class,'register']);
+Route::get('/recover',[\App\Http\Controller\indexController::class,'recover']);
+Route::get('/login',[\App\Http\Controller\indexController::class,'login']);
+Route::get('/logout',[\App\Http\Controller\indexController::class,'logout']);
+
