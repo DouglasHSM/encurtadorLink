b5bf206b12fa69030d083b9311751a0bea3bcb7e
“DouglasHSM” <doughenriqueps@gmail.com>
2024-07-04 00:30:27 -0300

    clear

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>view index</title>
-    
-</head>
-<body>
-    
-</body>
-</html>
+@extends('layouts.navbar')
+
+@section('titulo', 'Home')
+
+
+@section('conteudo')
+<h1> Olá </h1>
+@endsection
